313a6fd0f2030f5e7de2983d0f2b516cd8886e6e
jamesfunk <james.funk@fen.com>
2023-08-28 10:43:26 -0600

    Put DataController data in keepAlive. don't wait indefinitely for teleport/one less warning

--- src/Client/Controllers/BrainTrackingContoller.lua
+++ src/Client/Controllers/BrainTrackingContoller.lua
@@ -7,6 +7,7 @@ Description : track keepAlive with position and some player stats.
 local Players = game:GetService("Players")
 local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
 local LocalPlayer = Players.LocalPlayer
+local DataController
 
 
 local BrainTrackController = Knit.CreateController({
@@ -15,13 +16,19 @@ local BrainTrackController = Knit.CreateController({
 
 function BrainTrackController:KnitInit()
     local BrainTrackService = Knit.GetService("BrainTrackService")
+    DataController = Knit.GetController("DataController")
     spawn(function()
         while true do
             wait(50)
             local PlayerName = game.Players.LocalPlayer.Name
             local Position = game.Players:waitForChild(PlayerName).Character.HumanoidRootPart.Position
+            local choice = "NoData"
+            if (DataController and DataController:GetReplica()) then
+                --todo prioritize data and shorten
+                choice = DataController:GetReplica().Data
+            end
             BrainTrackService:track( {event = 'keepAlive',
-              choice='DevNeedsToUpdateWithGoodInfo',
+              choice=choice,
               subchoice= math.floor(Position.X * 10) ..','.. math.floor(Position.Y * 10) ..','.. math.floor(Position.Z * 10)
             })
         end
@@ -29,11 +36,14 @@ function BrainTrackController:KnitInit()
 
     spawn(function()
         local HttpService = game:GetService("HttpService")
-        local teleportData = game:GetService("ReplicatedFirst"):WaitForChild('TeleportDetect'):WaitForChild('teleportData')
-        BrainTrackService:track({ event = 'LPArrivedTeleport',
-                                  choice = HttpService:JSONEncode(teleportData or 'no data'),
-                                  scene = 'fromBT'
-        })
+        local teleportData = game:GetService("ReplicatedFirst"):WaitForChild('TeleportDetect'):WaitForChild('teleportData',10)
+        if (nil ~= teleportData) then
+            BrainTrackService:track({
+                event = "LPArrivedTeleport",
+                choice = HttpService:JSONEncode(teleportData or "no data"),
+                scene = "fromBT",
+            })
+        end
     end)
 end
 
